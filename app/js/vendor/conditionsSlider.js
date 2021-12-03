fd139a0ca6ec1f13aad85a6c888452b30e9d3cd7
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-12-04 02:07:53 +0300

    adaptive-more

--- app/js/vendor/conditionsSlider.js
+++ app/js/vendor/conditionsSlider.js
@@ -13,5 +13,20 @@ const conditionsSlider = new Swiper(conditionsSLiderWrapper, {
     nextEl: ".swiper-btn-next",
     prevEl: ".swiper-btn-prev",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    480: {
+      slidesPerView: 1.5,
+      centeredSlides:true,
+      spaceBetween: 30,
+    },
+    700: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 })
 });
